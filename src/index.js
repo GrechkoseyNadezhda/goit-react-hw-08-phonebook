206995e6f5bd21b71129eb1d95b96d340bacda26
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2023-01-24 07:52:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const theme = createTheme({
   },
 });
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw-08-phonebook">
     <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
